579b6b3af4560f60bbe9724d8f9827ca64c99c68
saikatnoman12 <noommaa240@gmail.com>
2022-10-07 17:42:21 +0600

    add js recursion

---
+++ recursion.js
@@ -0,0 +1,45 @@
+/* === JavScript Recursion === */
+
+//  use recursion;
+function countDown(number) {
+
+    console.log(number);
+
+    const newNumber = number - 1;
+
+    if (newNumber >= 0) {
+
+        countDown(newNumber);
+
+    }
+
+}
+
+countDown(10);
+
+
+// setInterVal use js recursion;
+function timeOut(time) {
+
+    // display time;
+    console.log(time);
+
+    const newTime = time - 1;
+
+    let interVal = setInterval(function () {
+
+        if (newTime >= 0) {
+
+            timeOut(newTime);
+
+        }
+
+        clearInterval(interVal);
+
+    }, 1000);
+
+}
+
+
+
+timeOut(12);
